e1fe479ce5496c5b820d7e07420c5d1fda3d1b27
Philippe Castonguay <ph.castonguay@gmail.com>
2015-06-07 15:17:20 -0400

    modulation

--- soundGenerator.py
+++ soundGenerator.py
@@ -4,14 +4,20 @@ import numpy as np
 
 
 
-
 def soundGenerator(freq = 10, stimDur = 1, f1 = 200, f2 = 400, vol = 1):
 	
 	p = pyaudio.PyAudio()
 	
 	fs = 44100       # sampling rate, Hz, must be integer
 	dur = 0.1  # in seconds, may be float
-	pauseTime = 0.1	
+	
+	freqEstim = np.asarray(range(1,100))
+	freqEstim = freqEstim/1000.0 + 0.01
+	
+	if freq >= 25:
+		freq = 24
+		
+	pauseTime = freqEstim[99-4*freq]
 	
 	#generate samples, note conversion to float32 array
 	sineWave1 = (np.sin(2*np.pi*np.arange(fs*dur)*f1/fs)).astype(np.float32)
