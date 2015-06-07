d8c741a02b170c475f79b2705b21c514fcd80f41
Philippe Castonguay <ph.castonguay@gmail.com>
2015-06-07 05:59:15 -0400

    m

--- csvMat.py
+++ csvMat.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 
-directory = '/home/phc/Dropbox/Hackathons/Brain-Soother/'
+#directory = '/home/phc/Dropbox/Hackathons/Brain-Soother/'
 fileName = 'OpenBCI-RAW-PHIL.txt'
 
-dataTxt = open(directory+fileName)
+dataTxt = open(fileName)
 dataString = dataTxt.read()
 
 lines = dataString.split('\n')
@@ -23,5 +24,18 @@ for l in lines[5:]:
 #	for j in range(0,len(data[i])-1):
 #		data[i][j] = float(data[i][j])
 		
-		
-			
+#FFT = np.fft.fft(data[1000:])
+#N = len(data)/2+1
+#
+##Time frequency domain
+#plt.figure()
+#plt.plot(abs(FFT[10:N]))
+#freq  = 250 #sampling frequency
+#
+#X = np.linspace(0, N*1/freq, N, endpoint=True)
+#
+#
+#plt.figure()
+#plt.plot(X, 2*np.abs(FFT[:N])/N)
+#plt.xlabel('Frequency ($Hz$)')
+#			
